efdb7fe6d65af82df76418cd9bec7aeee5a2ca29
Mangesh Kumar <mangeshkrm123@gmail.com>
2024-07-08 22:41:38 +0530

    bug fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full mb-[100px] pb-10" id="contact">
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
