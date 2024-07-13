e952c73b1632950ccbcdf15d6fdccb59bbb0e167
Mangesh Kumar <mangeshkrm123@gmail.com>
2024-07-13 17:21:22 +0530

    url fixes

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/utils/cn";
+import Link from "next/link";
 
 export const PinContainer = ({
   children,
@@ -12,7 +13,7 @@ export const PinContainer = ({
 }: {
   children: React.ReactNode;
   title?: string;
-  href?: string;
+  href: string;
   className?: string;
   containerClassName?: string;
 }) => {
@@ -28,7 +29,9 @@ export const PinContainer = ({
   };
 
   return (
-    <div
+    <Link
+      href={href}
+      target="_blank"
       className={cn(
         "relative group/pin z-50  cursor-pointer",
         containerClassName
@@ -53,7 +56,7 @@ export const PinContainer = ({
         </div>
       </div>
       <PinPerspective title={title} href={href} />
-    </div>
+    </Link>
   );
 };
 
@@ -62,13 +65,13 @@ export const PinPerspective = ({
   href,
 }: {
   title?: string;
-  href?: string;
+  href: string;
 }) => {
   return (
     <motion.div className="pointer-events-none w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
+          <Link
             href={href}
             target={"_blank"}
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10"
@@ -78,7 +81,7 @@ export const PinPerspective = ({
             </span>
 
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          </Link>
         </div>
 
         <div
