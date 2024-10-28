b866ba22c95f374aab5ee865bdfb71c7c315baa1
Mangesh Kumar <mangeshkrm123@gmail.com>
2024-10-28 11:30:33 +0530

    copyrigth year change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light mb-4">
-          Copyright © 2024 Mangesh Kumar
+          Copyright © 2025 Mangesh Kumar
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
